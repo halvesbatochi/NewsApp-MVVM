6ceeb47d7d538aadf9307ff881b138583a4ff83b
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-15 00:35:32 -0300

    Implementing Models

---
+++ NewsAppMVVM/NewsAppMVVM/Models/Article.swift
@@ -0,0 +1,17 @@
+//
+//  Article.swift
+//  NewsAppMVVM
+//
+//  Created by Henrique Alves Batochi on 15/10/22.
+//
+
+import Foundation
+
+struct ArticleResponse: Decodable {
+    let aarticles: [Article]
+}
+
+struct Article: Decodable {
+    let title: String
+    let description: String?
+}
