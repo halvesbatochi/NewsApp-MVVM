1d29fc7377667d88ad402c70fd8954f0df45c874
Henrique Alves Batochi <32850493+halvesbatochi@users.noreply.github.com>
2022-10-15 00:29:40 -0300

    Setting up the user interface

---
+++ NewsAppMVVM/NewsAppMVVM/Controllers/NewsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewsTableViewController.swift
+//  NewsAppMVVM
+//
+//  Created by Henrique Alves Batochi on 15/10/22.
+//
+
+import Foundation
+import UIKit
+
+class NewsTableViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+}
